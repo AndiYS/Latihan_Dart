beb28fad343f9b2a6be7047f3baf4cf453d25cda
AndiYS <muhammadyusril128@gmail.com>
2025-11-02 02:53:57 +0800

    first commit

---
+++ bin/newdart.dart
@@ -0,0 +1,15 @@
+//import 'package:newdart/newdart.dart' as newdart;
+//import 'dart:io' ;
+void main(){
+  print('Hello world: ${newdart.calculate()}!');
+  double umur ;
+  umur = 2 ;
+  print(umur) ;
+  stdout.write('Nama Anda : ');
+  String name = stdin.readLineSync()!;
+  stdout.write('Usia Anda : ');
+  int age = int.parse(stdin.readLineSync()!);
+  print('Halo $name, usia Anda $age tahun');
+  
+  
+}
